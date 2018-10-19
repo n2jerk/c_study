06266f6c7beb6536805a52cfaad1756f8547cbd0
n2jerk <2920904061@qq.com>
2018-10-19 20:00:22 +0800

    normal

--- aoapc/tmp/test.cpp
+++ aoapc/tmp/test.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 #include<vector>
 using namespace std;
+typedef struct  data
+{
+    int d;
+    struct data *l, *r;
+}node ;
+void print_tree(node b)
+{
+    if(b != NULL)
+    {
+        printf("%d ", b->d-'0');
+        if(b->l != NULL)
+            print_tree(b->l);
+        if(b->r != NULL)
+            print_tree(b->r);
+    }
+}
+node *create_tree(char a[], int n, int N)
+{
+    if (a[n] == '#' || n > N) return NULL;
+    node *b = (node *) malloc(sizeof(node));
+    b->d= a[n];
+    b->l = create_tree(a, 2 * n, N);
+    b->r = create_tree(a, 2 * n + 1, N);
+    return b;
+}
 int main(int argc, char const *argv[])
 {
-	int a[]={1,2,3};
-	int b=3;
-	bool s=typeid(b)==typeid(int);
+    char c[] = {'#', '1', '2', '3', '#', '4', '5', '6'};
+    node *b = create_tree(c, 1, 7);
+     print_tree(b);
     return 0;
 }
