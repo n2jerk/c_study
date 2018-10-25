140ccf70d7919117e54a8dee5d455bdc02ccc549
n2jerk <2920904061@qq.com>
2018-10-25 20:00:22 +0800

    normal

---
+++ luogu/1002.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#define N 21
+int  dp[N][N];
+//状态转移方程 dp[i][j]=dp[i-1][j]+dp[i][j-1]
+void mark_cell(int x,int y)
+{
+
+
+	
+}
+int main(int argc, char const *argv[])
+{
+	int m,n,x,y;
+	scanf("%d%d%d%d",&m,&n,&x,&y);
+
+	return 0;
+}
